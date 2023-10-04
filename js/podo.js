4c1a900f25c6d3a28715609529df13577c2244cf
kimhyeseung1 <hg724932@gmail.com>
2023-10-04 18:03:37 +0900

    feat: class 수정

--- js/podo.js
+++ js/podo.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", function () {
-    let modalWrap = document.querySelector(".modal-podo"); 
-    const podocheck = document.querySelector(".podocheck");
-    let modalClose = document.querySelector(".modal-close");
-    podocheck.addEventListener("click",function(){
-      modalWrap.style.display ="block";
-    })
-    modalClose.addEventListener("click" , function (){
-      modalWrap.style.display = "none";
-    })
-    })
-    
-    
+  let modalWrap = document.querySelector(".modal-podo"); 
+  const podocheck = document.querySelector(".podocheck");
+  let modalClose = document.querySelector(".modal-close");
+  podocheck.addEventListener("click",function(){
+    modalWrap.style.display ="block";
+  })
+  modalClose.addEventListener("click" , function (){
+    modalWrap.style.display = "none";
+  })
+  })
+  
+  
