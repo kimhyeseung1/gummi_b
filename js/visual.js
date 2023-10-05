665e51d1360e6ab43e2c8b8bfa5fbe48b2930dea
HamB20001013 <khb1535@gmail.com>
2023-10-05 09:26:56 +0900

    feat : visual 슬라이더 추가
    visual 슬라이더 추가

--- js/visual.js
+++ js/visual.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", function () {
-    
+  var swiper = new Swiper(".visual_swiper", {
+    autoHeight: true,
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+    slidesPerView: 1,
+    autoWidth: true,
+    autoHeight: true,
+
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+
+    pagination: {
+        el: ".visual_swiper .swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        prevEl: ".visual_swiper .swiper-button-prev",
+        nextEl: ".visual_swiper .swiper-button-next",
+    },
+  });
 });
